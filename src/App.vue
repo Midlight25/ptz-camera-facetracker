58cf79876a4defe7a2aaf837711b9d17b9e1b927
Michael Mesquita <mannolaur@gmail.com>
2021-08-18 19:48:15 -0400

    Added code to get URI stream from camera and it didn't trip CORS!

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,57 @@
 <template lang="pug">
 #main
   img(alt="Vue logo", src="./assets/logo.png")
-  h1 Hello There
-  p Welcome to the Jungle
+  h1 PTZ Camera Face Tracker
+
+  form(@submit.prevent)
+    label(for="camera-ip") PTZ Camera IP
+    input(type="text", name="camera-ip", placeholder="192.168.0.1", v-model="cameraIP")
+
+    button(@click="connectToCamera") Connect
+
+  h2(v-if="showURI") {{ streamURI }}
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
+import { Cam } from "onvif";
+
 
 export default defineComponent({
-  name: "App"
+  name: "App",
+  setup() {
+    // * Camera Discovery and Connection Logic
+    // Given IP of camera from user, attempt to connect to camera and report status.
+    const cameraIP = ref<string>("");
+    const showURI = ref<boolean>(false);
+    const streamURI = ref<string>("");
+
+    const connectToCamera = () => {
+      console.log("Connecting to Camera");
+
+      // Instantiate a camera object and get it's URI for accessing the media.
+      new Cam({
+        hostname: cameraIP.value,
+        username: "",
+        password: "",
+      }, function(err: Error): void {
+        this.absoluteMove({x: 0.5, y: 0, zoom: 0.5});
+        this.getStreamUri({protocol: "HTTP"}, function(err: Error, streamInfo): void {
+          streamURI.value = streamInfo.uri;
+        });
+      });
+
+      // Display the URI on the app
+      showURI.value = true;
+    }
+
+    return {
+      cameraIP,
+      showURI,
+      streamURI,
+      connectToCamera,
+    }
+  }
 })
 
 </script>
